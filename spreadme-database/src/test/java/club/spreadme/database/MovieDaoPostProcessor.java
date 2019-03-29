e780ed8020f63f445e5ca8cee4927e47952da580
wangShuwill <wangshuwei6@gmail.com>
2019-03-29 23:24:09 +0800

    optimization structure

---
+++ spreadme-database/src/test/java/club/spreadme/database/MovieDaoPostProcessor.java
@@ -0,0 +1,29 @@
+/*
+ *  Copyright (c) 2019 Wangshuwei
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package club.spreadme.database;
+
+import club.spreadme.database.annotation.PostProcessor;
+import club.spreadme.database.parser.grammar.SQLStatement;
+
+public class MovieDaoPostProcessor implements PostProcessor {
+
+    @Override
+    public SQLStatement process(SQLStatement sqlStatement) {
+        return null;
+    }
+
+}
