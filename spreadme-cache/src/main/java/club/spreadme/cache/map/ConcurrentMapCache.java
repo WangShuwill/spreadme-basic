19a95b138fe0c78adb95306e2ea1292f1937aed4
WangShuwill <897436004@qq.com>
2019-05-30 23:15:19 +0800

    spreadme frame core

--- spreadme-cache/src/main/java/club/spreadme/cache/map/ConcurrentMapCache.java
+++ spreadme-cache/src/main/java/club/spreadme/cache/map/ConcurrentMapCache.java
@@ -1,27 +1,28 @@
 /*
- *  Copyright (c) 2019 Wangshuwei
+ * Copyright (c) 2019 Wangshuwei
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License
  */
 
-package club.spreadme.basic.cache.support;
+package club.spreadme.cache.map;
+
+import club.spreadme.cache.core.Cache;
+import club.spreadme.cache.core.ValueLoader;
+import club.spreadme.core.utils.Assert;
 
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
-import club.spreadme.basic.cache.Cache;
-import club.spreadme.basic.cache.ValueLoader;
-import club.spreadme.basic.utils.Assert;
 
 public class ConcurrentMapCache<K, V> implements Cache<K, V> {
 
