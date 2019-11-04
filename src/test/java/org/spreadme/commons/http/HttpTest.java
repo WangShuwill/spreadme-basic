94bfa72b1d178783e2f2dbfcb512ba6acb67fd5f
shuwill <wangshuwei6@gmail.com>
2019-11-04 21:58:16 +0800

    user-agent

---
+++ src/test/java/org/spreadme/commons/http/HttpTest.java
@@ -0,0 +1,35 @@
+/*
+ *    Copyright [2019] [shuwei.wang (c) wswill@foxmail.com]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.spreadme.commons.http;
+
+import org.junit.Test;
+import org.spreadme.commons.http.useragent.Platform;
+import org.spreadme.commons.http.useragent.UserAgent;
+
+/**
+ * @author shuwei.wang
+ */
+public class HttpTest {
+
+	@Test
+	public void testUserAgent() {
+		final String useragent = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1";
+		Platform platform = UserAgent.getPlatform(useragent);
+		System.out.println("IsMobile: " + platform.isMobile());
+		System.out.println("Platfrom: " + platform.getName());
+	}
+}
