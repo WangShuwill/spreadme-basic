1269a92f8afbde2e12aec338d5cdee0f8b25a446
shuwill <wangshuwei6@gmail.com>
2019-11-19 22:12:57 +0800

    native lib loader and console instead of System.out.println method

---
+++ src/main/java/org/spreadme/commons/lang/Console.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright [2019] [shuwei.wang (c) wswill@foxmail.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.spreadme.commons.lang;
+
+import java.util.Date;
+
+/**
+ * Console instead of system out
+ * @author shuwei.wang
+ */
+public abstract class Console {
+
+	public static void info(Object object, Object... args) {
+		System.out.println("INFO[" + Dates.format(new Date(), Dates.DateFormatType.NORM_DATETIME_PATTERN) +
+				"] " + String.format(object.toString(), args));
+	}
+
+}
