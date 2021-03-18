02423662a7e8386c35c4af3349e6323ea233ae0a
shuwei.wang 王淑伟 <shuwei.wang@qiyuesuo.com>
2021-03-18 14:18:48 +0800

    thead utils

--- src/main/java/org/spreadme/commons/thread/NamedThreadFactory.java
+++ src/main/java/org/spreadme/commons/thread/NamedThreadFactory.java
@@ -1,2 +1,28 @@
-package org.spreadme.commons.thread;public class NamedThreadFactory {
+package org.spreadme.commons.thread;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class NamedThreadFactory implements ThreadFactory {
+
+    private final ThreadGroup group;
+    private final String name;
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+
+    public NamedThreadFactory(String name) {
+        SecurityManager s = System.getSecurityManager();
+        this.group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+        this.name = name;
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(group, r, name + "-thread-" + threadNumber.getAndIncrement(), 0);
+        if (t.isDaemon())
+            t.setDaemon(false);
+        if (t.getPriority() != Thread.NORM_PRIORITY)
+            t.setPriority(Thread.NORM_PRIORITY);
+        return t;
+    }
+
 }
--- src/main/java/org/spreadme/commons/thread/ThreadUtils.java
+++ src/main/java/org/spreadme/commons/thread/ThreadUtils.java
@@ -1,2 +1,18 @@
-package org.spreadme.commons.thread;public class ThreadUtils {
+package org.spreadme.commons.thread;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class ThreadUtils {
+
+    public static void sleep(long timeout, TimeUnit timeunit) {
+        try {
+            timeunit.sleep(timeout);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static String getThreadName() {
+        return Thread.currentThread().getName();
+    }
 }
